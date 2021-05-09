3b126c0264b331b7b9da0b54ce9064df2759e220
athornton95 <48295302+athornton95@users.noreply.github.com>
2021-05-09 17:30:46 -0600

    color update

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -14,7 +14,7 @@ const Block = (props) => {
       </div>
       <div className='max-w-3xl text-center mx-auto'>
         <p className='section-header pt-2 pl-0 sm:pt-0 block sm:inline font-display self-end'>Get in Touch</p>
-        <p className='mb-12 text-textSecondary text-lg pt-4'>Although I'm not currently looking for work, I'm always happy to chat and entertain exciting opportunties. If you want to start a conversation,
+        <p className='mb-12 text-textTertiary text-lg pt-4'>Although I'm not currently looking for work, I'm always happy to chat and entertain exciting opportunties. If you want to start a conversation,
           please shoot me an email or connect with me on LinkedIn and we can grab a coffee (virtually or in person).
         </p>
         <a href="mailto:ahopkins.co@gmail.com" className='text-textSecondary font-title border border-textPrimary px-6 py-4 rounded-md hover:bg-textPrimary hover:text-bgPrimary'>Say hello</a>

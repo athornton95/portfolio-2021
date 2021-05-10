b4cf70f7f50fc7262d8f14f5701cee5f41848f92
athornton95 <48295302+athornton95@users.noreply.github.com>
2021-05-10 14:54:04 -0600

    remove cached stuff

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/alexhopkins/Github/personal/portfolio-2021/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/alexhopkins/Github/personal/portfolio-2021/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/alexhopkins/Github/personal/portfolio-2021/src/pages/index.js"))
 }

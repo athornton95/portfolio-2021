3718f8b5e6aa246517ebc1449a5d9ef2e4db66b1
athornton95 <48295302+athornton95@users.noreply.github.com>
2021-05-10 17:18:36 -0600

    update text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,7 +11,7 @@ const Hero = () => (
       </p>
       <p className='text-textTertiary font-display mb-12 sm:max-w-xl'>
         I'm a Denver based Frontend Developer with a background in design, research, and language.
-        I specialize in developing (and now and again designing) delighful user experiences.
+        I specialize in developing (and now and again designing) delightful user experiences.
         Currently, I'm a Web Engineer at <span className='btn-inline'><a href='https://gloo.us' target='_blank' rel="noopener noreferrer">Gloo</a></span>, focused on creating human-centered products in the personal growth space.
       </p>
       <a href="mailto:ahopkins.co@gmail.com" className='text-textSecondary font-title border border-textPrimary px-6 py-4 rounded-md hover:bg-textPrimary hover:text-bgPrimary'>Get in touch</a>
